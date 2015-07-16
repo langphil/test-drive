855e1187d6faa6b94ec00d831c91c8a729e1d85f
pikesley <github@orgraphone.org>
2015-07-16 16:13:29 +0100

    First refactor

--- lib/tdd/demo/test_drive.rb
+++ lib/tdd/demo/test_drive.rb
@@ -2,9 +2,7 @@ module Tdd
   module Demo
     class TestDrive
       def TestDrive.is_bigger first, second
-        if first == 4
-          return false
-        elsif first == 2
+        if first < second
           return false
         else
           return true
