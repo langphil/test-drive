e5ffb3459351852d057603db1a1a901c0fcd484a
pikesley <github@orgraphone.org>
2015-07-16 16:07:41 +0100

    Test fails

--- spec/lib/tdd/demo/test_drive_spec.rb
+++ spec/lib/tdd/demo/test_drive_spec.rb
@@ -5,6 +5,10 @@ module Tdd
         it 'knows that 3 is bigger than 2' do
           expect(TestDrive.is_bigger 3, 2).to eq true
         end
+
+        it 'knows that 4 is smaller than 7' do
+          expect(TestDrive.is_bigger 4, 7).to eq false
+        end
       end
     end
   end
