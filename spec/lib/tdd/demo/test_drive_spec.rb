5525234ea851ea231a50c3ebc787ad0236b38851
pikesley <github@orgraphone.org>
2015-07-20 11:24:34 +0100

    Second test fails

--- spec/lib/tdd/demo/test_drive_spec.rb
+++ spec/lib/tdd/demo/test_drive_spec.rb
@@ -5,6 +5,10 @@ module Tdd
         it 'knows that 3 plus 5 is 8' do
           expect(TestDrive.add 3, 5).to eq 8
         end
+
+        it 'knows that 12 plus 19 is 31' do
+          expect(TestDrive.add 12, 19).to eq 31
+        end
       end
     end
   end
