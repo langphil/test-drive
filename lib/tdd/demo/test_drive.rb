096aa62cec7bbb4a66f5356c338f774ff7e41b82
pikesley <github@orgraphone.org>
2015-07-20 11:37:57 +0100

    Git is confusing

--- lib/tdd/demo/test_drive.rb
+++ lib/tdd/demo/test_drive.rb
@@ -1,8 +1,8 @@
 module Tdd
   module Demo
     class TestDrive
-      def TestDrive.is_bigger first, second
-        return first > second
+      def TestDrive.add first, second
+        return first + second
       end
     end
   end
--- spec/lib/tdd/demo/test_drive_spec.rb
+++ spec/lib/tdd/demo/test_drive_spec.rb
@@ -1,21 +1,21 @@
 module Tdd
   module Demo
     describe TestDrive do
-      context 'Comparisons' do
-        it 'knows that 3 is bigger than 2' do
-          expect(TestDrive.is_bigger 3, 2).to eq true
+      context 'Addition' do
+        it 'knows that 3 plus 5 is 8' do
+          expect(TestDrive.add 3, 5).to eq 8
         end
 
-        it 'knows that 4 is smaller than 7' do
-          expect(TestDrive.is_bigger 4, 7).to eq false
+        it 'knows that 12 plus 19 is 31' do
+          expect(TestDrive.add 12, 19).to eq 31
         end
 
-        it 'knows that 2 is smaller than 19' do
-          expect(TestDrive.is_bigger 2, 19). to eq false
+        it 'knows that 12 plus 70 is 82' do
+          expect(TestDrive.add 12, 70).to eq 82
         end
 
-        it 'knows that Z is bigger than A' do
-          expect(TestDrive.is_bigger 'Z', 'A'). to eq true
+        it 'knows that 13 plus 105 is 118' do
+          expect(TestDrive.add 13, 105).to eq 118
         end
       end
     end
