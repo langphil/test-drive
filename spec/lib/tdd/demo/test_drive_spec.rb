cd0df4177226b4729dec595b17c7f16a95939e2d
pikesley <github@orgraphone.org>
2015-07-20 11:10:12 +0100

    Fix words

--- spec/lib/tdd/demo/test_drive_spec.rb
+++ spec/lib/tdd/demo/test_drive_spec.rb
@@ -1,7 +1,7 @@
 module Tdd
   module Demo
     describe TestDrive do
-      context 'Comparisons' do
+      context 'Addition' do
         it 'knows that 3 plus 5 is 8' do
           expect(TestDrive.add 3, 5).to eq 8
         end
