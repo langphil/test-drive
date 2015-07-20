453050a2a08f26031d1d8dae2ee713a6e220c1f8
pikesley <github@orgraphone.org>
2015-07-20 11:25:22 +0100

    Second test passes

--- lib/tdd/demo/test_drive.rb
+++ lib/tdd/demo/test_drive.rb
@@ -2,7 +2,11 @@ module Tdd
   module Demo
     class TestDrive
       def TestDrive.add first, second
-        return 8
+        if first.is 12
+          return 31
+        else
+          return 8
+        end
       end
     end
   end
