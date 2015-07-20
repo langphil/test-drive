5607e0e43b80c36712155bbffa8e23ffacc5c88d
pikesley <github@orgraphone.org>
2015-07-20 11:28:37 +0100

    Refactor

--- lib/tdd/demo/test_drive.rb
+++ lib/tdd/demo/test_drive.rb
@@ -2,15 +2,7 @@ module Tdd
   module Demo
     class TestDrive
       def TestDrive.add first, second
-        if first.is 12
-          if second.is 70
-            return 82
-          else
-            return 31
-          end
-        else
-          return 8
-        end
+        return first + second
       end
     end
   end
